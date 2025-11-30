e553b84917a9748b0516946756e07093e5f679db
karthik13vk <vijayakarthik13@gmail.com>
2025-11-30 13:11:41 +0530

    rename

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import Header from './pages/component/Header/Header'
 import Banner from './pages/component/Banner/Banner'
-import Guidecard from './pages/component/guidecard/Guidecard'
+// import Guidecard from './pages/component/Guidecard/Guidecard'
 import Application from './pages/component/Application/Application'
 import { Services } from './pages/component/Services/Services'
 import TagPart from './pages/component/Tag-sec/TagPart'
 import Community from './pages/component/Community/Community'
 import Footer from './pages/component/Footer/Footer'
+import Guidecard from './pages/component/Guidecard/Guidecard'
 
 function App() {
   return (
